85a5a0dcb7780c73e74ed34c710449013d323e55
Kyle Geary <gearykye34@gmail.com>
2024-03-01 11:13:30 -0700

    added

--- server/config.py
+++ server/config.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from sqlalchemy import MetaData
 import secrets
 from dotenv import load_dotenv
@@ -15,12 +16,12 @@ load_dotenv()
 
 
 # deployed version uncomment below code, local version comment out below code
-# app = Flask(
-#     __name__,
-#     static_url_path='',
-#     static_folder='../client/dist',
-#     template_folder='../client/dist'
-# )
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/dist',
+    template_folder='../client/dist'
+)
 
 # Instantiate app, set attributes
 
@@ -29,7 +30,7 @@ load_dotenv()
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test_app_vnog_user:KHoqCYV9C8lN13JLB88C43GlSrPwQ761@dpg-cnh1aki1hbls738c6rag-a.ohio-postgres.render.com/test_app_vnog'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 app.secret_key = secrets.token_hex(16)
